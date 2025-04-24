6dbd162bfa820b8ec842c5bb9b3264b2b879b930
Gavin Bardwell <gmbwe110@gmail.com>
2025-04-24 18:14:45 -0500

    current final solution
    It should work. yolo small updates in update

--- zsim/src/hawkeye_repl.h
+++ zsim/src/hawkeye_repl.h
@@ -27,28 +27,16 @@ class HawkeyeReplPolicy : public ReplPolicy {
         int64_t* prevPC;    //tracks the last pc to access the respective rrip
         bool* wasReplaced;  //tracks if the id is to be replacedd
 
-<<<<<<< Updated upstream
-=======
         // new: per‐line recency tracking, for LRU portion
         uint64_t* lastAccess;
         uint64_t  accessCounter;
 
->>>>>>> Stashed changes
         vector<optgen> opt_sims;               //include an optgen fro each set
         hawkeye_predictor predictor;
 
 
     public:
         explicit HawkeyeReplPolicy(uint32_t _numSets, uint32_t _numWays, uint32_t _lineSize) 
-<<<<<<< Updated upstream
-            : numSets(_numSets), numWays(_numWays), predictor() {
-            //set up the rrip portion
-            array = gm_calloc<int8_t>(numSets * numWays);
-            wasReplaced = gm_calloc<bool>(numSets * numWays);
-            for(uint32_t i = 0; i < (uint32_t)numSets * numWays; i++) {
-                array[i] = CACHE_AVERSE;
-                wasReplaced[i] = false;
-=======
             : numSets(_numSets), numWays(_numWays), accessCounter(0), predictor() {
             //set up the rrip portion
             array = gm_calloc<int8_t>(numSets * numWays);
@@ -58,7 +46,6 @@ class HawkeyeReplPolicy : public ReplPolicy {
                 array[i] = CACHE_AVERSE;
                 wasReplaced[i] = false;
                 lastAccess[i]  = 0;
->>>>>>> Stashed changes
             }
 
 
@@ -77,97 +64,23 @@ class HawkeyeReplPolicy : public ReplPolicy {
             gm_free(prevPC);
             gm_free(array);
             gm_free(wasReplaced);
-<<<<<<< Updated upstream
-            /*for(uint32_t i = 0; i < numSets; i++) {
-                opt_sims[i].~optgen();
-            }
-            gm_free(opt_sims);*/
-        }
-
-        void update(uint32_t id, const MemReq* req) {
-            assert(id < numSets * numWays);
-            //need the set to know which optgen to access
-            uint32_t set = calculate_set_idx(req->lineAddr);
-            assert(set < numSets);
-=======
             gm_free(lastAccess);
         }
 
         void update(uint32_t id, const MemReq* req) {
             //need the set to know which optgen to access
             uint32_t set = calculate_set_idx(req->lineAddr);
->>>>>>> Stashed changes
 
             //log the cache interaction, update the optgen
             int32_t response = opt_sims[set].cache_access(req);
             //do not train if this is the first time seeing this 
             if(response != -1) { 
-<<<<<<< Updated upstream
-                uint64_t last = opt_sims[set].find_last_pc(req->lineAddr);
-                predictor.train_instruction(last, response);
-=======
                 uint64_t lastPC = opt_sims[set].find_last_pc(req->lineAddr);
                 predictor.train_instruction(lastPC, response);
->>>>>>> Stashed changes
             }
             //predict the new cache value to be in the RRIP
             bool pred_friendly = predictor.predict_instruction(req->pc);
             
-<<<<<<< Updated upstream
-            if(response == 1) {
-                array[id] = CACHE_FRIENDLY;
-            }
-            else {
-                if(wasReplaced[id] && array[id] < CACHE_AVERSE) {
-                    predictor.train_instruction(prevPC[id], 0); //detrain the value if cache friendly
-                    // age just this one set
-                    uint32_t base = set * numWays;
-                    for (uint32_t w = 0; w < numWays; ++w) {
-                    uint32_t idx = base + w;
-                    if (idx != id && array[idx] < CACHE_AVERSE - 1)
-                        array[idx]++;
-                    }
-                }
-            }
-
-            prevPC[id] = req->pc;
-            array[id] = pred_friendly ? CACHE_FRIENDLY : CACHE_AVERSE;
-            wasReplaced[id] = false;
-        }
-        void replaced(uint32_t id) {
-            assert(id < numSets * numWays);
-            wasReplaced[id] = true;
-        }
-
-        //find a victim, uses RRIP
-        template <typename C> inline uint32_t rank(const MemReq* req, C cands) {
-            //search for a cache adverse candidate
-            for (auto ci = cands.begin(); ci != cands.end(); ci.inc()) {
-                assert(*ci < numSets * numWays);
-                if(array[*ci] == (int8_t)CACHE_AVERSE) {
-                    return *ci;// Evict first candidate with max RRP
-                }
-            }
-            //if no cache adverse lines are found... look for the next max one
-            for(uint32_t i = 0; i <= CACHE_AVERSE; i++) {
-                //search for a max value cache friedly one
-                for (auto ci = cands.begin(); ci != cands.end(); ci.inc()) {
-                    assert(*ci < numSets * numWays);
-                    if(array[*ci] == (int8_t)(CACHE_AVERSE - 1)) {
-                        return *ci;// Evict first candidate with max RRP
-                    }
-                }
-                //no match found increment all status
-                for (auto ci = cands.begin(); ci != cands.end(); ci.inc()) {
-                    assert(*ci < numSets * numWays);
-                    //cache adverse tag can only set by the predictor
-                    if(array[*ci] < (int8_t)(CACHE_AVERSE-1))
-                        array[*ci]++;
-                }
-            }
-            panic("error no rank found\n");
-            return -1;
-=======
             if(wasReplaced[id]) {//if cache miss 
                 if(array[id] < CACHE_AVERSE) //if the cache line to be replaced was friendly
                     predictor.train_instruction(prevPC[id], 0);//detrain the entry
@@ -211,15 +124,10 @@ class HawkeyeReplPolicy : public ReplPolicy {
                 }
             }
             return victim;
->>>>>>> Stashed changes
         }
         DECL_RANK_BINDINGS;
 
 
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
     private:
         uint32_t calculate_set_idx(uint32_t lineAddr) {
             uint32_t setIdx = lineAddr >> blockOffsetBits;
